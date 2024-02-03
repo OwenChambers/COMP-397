10a00ca47cc5dae6ae4118ed75318e7b3e4597e5
OwenChambers <owen.chambers2001@gmail.com>
2024-02-02 23:22:32 -0500

    Lab #3 Final
    Added Menu and Lighting

--- COMP 397/Assets/Scripts/MenuController.cs
+++ COMP 397/Assets/Scripts/MenuController.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
+    TMP_Dropdown sceneSelector;
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        sceneSelector = GetComponentInChildren<TMP_Dropdown>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(sceneSelector.value);
     }
 }
